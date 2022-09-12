53317073f005b9ea94445eea3dde1ccaddb4ee10
quling <quling@dist.com.cn>
2022-09-12 16:03:53 +0800

    成都市Geometry增加点击事件,并显示点击的位置

--- src/components/map/utils/AdvanceViewPosition.js
+++ src/components/map/utils/AdvanceViewPosition.js
@@ -2,13 +2,14 @@
  * @Author: quling
  * @Date: 2022-09-11 11:15:28
  * @LastEditors: quling
- * @LastEditTime: 2022-09-11 16:54:58
+ * @LastEditTime: 2022-09-12 15:59:09
  * @Description: 高级视口定位
  */
 import VectorSource from 'ol/source/Vector'
 import GeoJSON from 'ol/format/GeoJSON'
 import VectorLayer from 'ol/layer/Vector'
-import {Fill, Stroke, Style} from 'ol/style'
+import { Fill, Stroke, Style } from 'ol/style'
+import Select from 'ol/interaction/Select'
 
 export default {
   data () {
@@ -38,6 +39,33 @@ export default {
         source: this.source,
         style: style
       })
+
+      const select = new Select({
+        layers: [layer],
+        style: new Style({
+          fill: new Fill({
+            color: '#1fee00'
+          }),
+          stroke: new Stroke({
+            color: '#568920',
+            width: 2
+          }),
+          zIndex: 1
+        })
+      })
+
+      select.on('select', (e) => {
+        // console.log('select', e)
+        const features = e.selected
+        console.log(features)
+        const extent = features[0].getGeometry().getExtent()
+        console.log(extent)
+        const name = features[0].get('name')
+        this.selectPosition = name
+        this.map.getView().fit(extent)
+      })
+
+      this.map.addInteraction(select)
 
       this.map.addLayer(layer)
     },
